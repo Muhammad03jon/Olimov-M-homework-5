48f77db270ece261219129b85c400726037d62f3
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-02-27 16:47:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,25 @@ import seaborn as sns
 
 st.title('Предсказание реальной или фальшивой банкноты')
 
-file_path = "data_banknote_authentication.txt"
+file_path = "https://raw.githubusercontent.com/Muhammad03jon/Olimov-M-homework-5/refs/heads/master/data_banknote_authentication.txt"
 
 with open(file_path, "r") as file:
     lines = file.readlines()
 
 df = pd.read_csv(file_path, sep=",", header=None)
 df.columns = ["variance", "skewness", "curtosis", "entropy", "class"]
+
+with st.expander('Data'):
+    col1, col2 = st.columns(2)  # Две колонки
+
+    with col1:
+        st.subheader("X (Features)")
+        X_raw = df.drop('class', axis=1)
+        st.dataframe(X_raw.style.set_properties(**{'background-color': '#f0f2f6', 'color': 'black'}))
+
+    with col2:
+        st.subheader("y (Target)")
+        y_raw = df.class
+        st.dataframe(y_raw.to_frame().style.set_properties(**{'background-color': '#e8f4ea', 'color': 'black'}))
+
 

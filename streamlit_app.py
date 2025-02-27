33879554e72e18a84e81c1a9be772c6bbb4f8466
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-02-27 16:35:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,11 @@ import seaborn as sns
 
 st.title('Предсказание реальной или фальшивой банкноты')
 
+file_path = "data_banknote_authentication.txt"
+
+with open(file_path, "r") as file:
+    lines = file.readlines()
+
+df = pd.read_csv(file_path, sep=",", header=None)
+df.columns = ["variance", "skewness", "curtosis", "entropy", "class"]
 
